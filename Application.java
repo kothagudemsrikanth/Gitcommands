e76b405d37aef107394b8de35db0acbd36ad6ab0
kothagudemsrikanth <srikanth.raj72@gmail.com>
2024-08-24 10:47:39 +0530

    App.java update-1

--- Application.java
+++ Application.java
@@ -22,3 +22,23 @@ public class SwapNumbers {
         System.out.println("Second number = " + second);
     }
 }
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 12.0f, second = 24.5f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        first = first - second;
+        second = first + second;
+        first = second - first;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
