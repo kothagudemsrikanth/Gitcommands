2e1ace264cf4475c49ab10b77a4e798519afe3a0
kothagudemsrikanth <srikanth.raj72@gmail.com>
2024-08-24 11:01:58 +0530

    Update-2

--- Application.java
+++ Application.java
@@ -23,6 +23,24 @@ public class SwapNumbers {
     }
 }
 
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
+
 public class SwapNumbers {
 
     public static void main(String[] args) {
